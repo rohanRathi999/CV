4c808f1e2055bc60247df4fcb99d369c2586334d
Harsh Keshari <108923011+hrshkshri@users.noreply.github.com>
2022-10-05 23:32:44 +0530

    Add files via upload

---
+++ CPP Templete/templete.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define l long
+#define ld long double
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long>
+#define pb push_back
+#define sp <<" "<<
+
+void yes() { cout<<"YES\n";}
+void no() { cout<<"NO\n";}
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        
+    }
+    return 0;
+}
